7f539e04b0135b063e01f909b16bce01b2b1c060
Reggie Basinal <34482113+gbasinal@users.noreply.github.com>
2024-02-22 17:17:31 -0700

    added set up files and configs

---
+++ src/model/character.model.ts
@@ -0,0 +1,11 @@
+export interface CharacterModel {
+    name : string;
+    stats : {
+        strength : number,
+        agility : number,
+        intelligence : number,
+        vitality : number,
+        luck : number
+    }
+  }
+  
---
+++ src/services/character-creation.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { CharacterModel } from 'src/model/character.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterCreationService {
+
+  constructor(
+    private storage : Storage
+  ) { 
+    this.init();
+  }
+
+  async init() {
+    await this.storage.create();
+  }
+
+  async saveStatAndName( stats:  CharacterModel ){
+    await this.storage.set('characterStats', stats);
+  }
+
+  async getStatsAndName() : Promise<CharacterModel> {
+    return await this.storage.get("characterStats");
+  }
+}
